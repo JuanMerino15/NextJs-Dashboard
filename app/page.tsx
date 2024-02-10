2349030609be35279100cb25cd0b1aa6753eba54
José Juan Rivera Merino <juanrivmerino4@gmail.com>
2024-02-09 18:43:21 -0600

    capitulo 4 diseños y paginas

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ import { lusitana } from '@/app/ui/fonts';
 import styles from '@/app/ui/home.module.css';
 import Image from 'next/image';
 
+
+
 export default function Page() {
+  
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
